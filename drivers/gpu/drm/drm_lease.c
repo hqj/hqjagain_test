1b275d33cad994a218e54fc936f94957f27b15ff
hqj <16095093@qq.com>
2020-03-15 18:09:17 +0800

    drm/lease: fix warning in idr_destroy
    drm/lease: fix warning in idr_destroy

--- drivers/gpu/drm/drm_lease.c
+++ drivers/gpu/drm/drm_lease.c
@@ -577,6 +577,10 @@ int drm_mode_create_lease_ioctl(struct drm_device *dev,
 
 out_lessee:
 	drm_master_put(&lessee);
+	put_unused_fd(fd);
+	
+	DRM_DEBUG_LEASE("drm_mode_create_lease_ioctl failed: %d\n", ret);
+	return ret;
 
 out_leases:
 	put_unused_fd(fd);
